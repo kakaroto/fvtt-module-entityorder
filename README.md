097a82721adca5fa0d09c2e3617faf558e80c0cd
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2019-06-23 15:36:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Entity Order
+
+This Foundry VTT module allows you to re-order entities (Actors, Scenes, Items and Journal entries).
+
+# Installation
+Extract the entityorder.zip file to the public/modules directory. As DM go to the `Manage Modules` options menu in your World ('?' icon on the side bar) then enable the `Entity Order` module.
+
+# Using it
+Just drag and drop your entities and they should get placed wherever you drop them. To place one at the top of a folder, drop it onto the folder name itself. An entity will always be placed after the entity you drop it onto.
+
+# License
+This Foundry VTT module, writen by KaKaRoTo, is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
+
+This work is licensed under Foundry Virtual Tabletop [EULA - Limited License Agreement for module development v 0.1.6](http://foundryvtt.com/pages/license.html).
---
+++ entityorder.js
@@ -0,0 +1,90 @@
+var pendingRender = false
+function ensureOrder(entity) {
+    let order = entity.getFlag("entityorder", "order");
+    if (order === undefined) {
+	entity.setFlag("entityorder", "order", entity.collection.entities.indexOf(entity))
+    }
+}
+function sortEntities(a, b) {
+    let order_a = a.getFlag("entityorder", "order");
+    let order_b = b.getFlag("entityorder", "order");
+    return order_a - order_b;
+}
+
+function cleanFolderName(folder) {
+    folder.name = folder.name.replace(/^•[0-9]+•/, "")
+    folder.children.forEach(cleanFolderName)
+}
+
+Folder._entityorder_original_setupFolders = Folder.setupFolders;
+Folder.setupFolders = function(entityType, entities) {
+    entities.forEach(ensureOrder)
+    sorted_entities = entities.sort(sortEntities)
+    let [tree, root_entities] = this._entityorder_original_setupFolders(entityType, sorted_entities);
+    tree.forEach(cleanFolderName)
+    return [tree, root_entities]
+}
+
+SidebarDirectory.prototype._entityorder_original_handleDropData = SidebarDirectory.prototype._handleDropData;
+SidebarDirectory.prototype._handleDropData = function(event, data) {
+    console.log("Handle drop data : ", event, data);
+    let before = $(event.target).closest(".directory-item");
+    let after = $(event.target).next(".directory-item");
+    console.log("Going to place item between ", before, " and ", after);
+    let ent = this.constructor.collection.get(data.id);
+    // ent.setFlag("entityorder", "order", (before + after) / 2 )
+
+    folder = $(event.target).closest(".folder");
+    if ( folder.length > 0 ) {
+	folder_id = folder.attr("data-folder-id");
+    } else {
+	folder_id = null;
+    }
+    var new_order = ""
+    if (before.length != 0 && after.length == 0) {
+	after = before.next(".directory-item")
+    }
+    if (before.length == 0 && after.length == 0) {
+	children = folder.find(".directory-item")
+	// Added to the beginning
+	if (children.length == 0) {
+	    new_order = 0
+	} else {
+	    before = $(children[0])
+	    let before_ent = this.constructor.collection.get(before.attr("data-entity-id"))
+	    new_order = before_ent.getFlag("entityorder", "order") -1
+	}
+    } else if (before.length != 0 && after.length == 0) {
+	let before_ent = this.constructor.collection.get(before.attr("data-entity-id"))
+	
+	// Added to the end
+	new_order = before_ent.getFlag("entityorder", "order") + 1
+    } else {
+	let before_ent = this.constructor.collection.get(before.attr("data-entity-id"))
+	let after_ent = this.constructor.collection.get(after.attr("data-entity-id"))
+	
+	// Added in between
+	new_order = (before_ent.getFlag("entityorder", "order") + after_ent.getFlag("entityorder", "order")) / 2
+    }
+    console.log("New order = ", new_order)
+    ent.setFlag("entityorder", "order", new_order);
+    if (ent.data.folder != folder_id) {
+	console.log("Folder is going to get updated from ", ent.data.folder, " to " , folder_id);
+	//ent.setFlag("entityorder", "order", "");
+    } else {
+	pendingRender = true
+    }
+    return this._entityorder_original_handleDropData(event, data)
+}
+
+function entityUpdated(obj) {
+    if (pendingRender) {
+	obj.collection.render()
+    }
+    pendingRender = false
+}
+
+Hooks.on('updateJournalEntry', entityUpdated)
+Hooks.on('updateScene', entityUpdated)
+Hooks.on('updateActor', entityUpdated)
+Hooks.on('updateItem', entityUpdated)
