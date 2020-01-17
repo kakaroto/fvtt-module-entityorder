19a9e86ad42a9a3b022575c7d6d8d13b948a761f
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2020-01-17 16:18:27 -0500

    Update for 0.4.4

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This module was previously used to re-order entities (Actors, Scenes, Items and 
 
 Most of its functionality has now been integrated into the core features of FVTT, and the 'sort folder' feature was moved into the [Furnace](https://github.com/kakaroto/fvtt-module-furnace) module.
 
-This module continues to exist only so it can migrate the entities' order value from the module flags into the core database.
+This module continues to exist only so it can migrate the entities' order value from the module flags into the core database. Once it has done that, it will automatically disable itself.
 
 # Installation
 In the setup screen, use the URL `https://raw.githubusercontent.com/kakaroto/fvtt-module-entityorder/master/module.json` to install the module.
@@ -18,7 +18,7 @@ As DM go to the `Manage Modules` options menu in your World then enable the `Ent
 
 The first time you open your world, this module will convert the saved entity order from its own settings into the core sort order which can take quite a long time as every entity would need to be updated. Please be patient while that happens as it will slow down your FVTT until it finishes the migration process.
 
-After it is done, the module can be disabled as it has no other uses anymore.
+After it is done, the module will be disabled as it has no other uses anymore.
 
 # License
 This Foundry VTT module, writen by KaKaRoTo, is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
--- entityorder.js
+++ entityorder.js
@@ -1,69 +1,28 @@
 class EntityOrder {
-
-	// Apparently that's how we do consts inside classes...
-	static get ORDER_MUL() {
-		return 100000;
-	}
-
-	static getEntityFolderContext(html, options) {
-		options.push({
-			name: "Sort Alphabetically (Ascending)",
-			icon: '<i class="fas fa-sort-alpha-down"></i>',
-			condition: game.user.isGM,
-			callback: header => EntityOrder.sortEntities(header, true)
-		})
-		options.push({
-			name: "Sort Alphabetically (Descending)",
-			icon: '<i class="fas fa-sort-alpha-down-alt"></i>',
-			condition: game.user.isGM,
-			callback: header => EntityOrder.sortEntities(header, false)
-		})
-	}
-
-
-	// Re-order the entire list based on their position.
-	static async sortEntities(header, ascending) {
-		let folderId = header.parent().attr("data-folder-id");
-		let folder = game.folders.get(folderId);
-		let entities = folder.content;
-
-		// Reset order values according to the new order within this folder
-		// This won't affect the order with the other folders and the whole collection
-		// order will be reset on the next render
-		entities.sort((a, b) => a.data.name.localeCompare(b.data.name) * (ascending ? 1 : -1))
-		for (let i = 0; i < entities.length; i++) {
-			let order = i * EntityOrder.ORDER_MUL;
-			await entities[i].update({ sort: order });
-		}
-	}
-
 	// Use the new 'sort' field for all entities that was introduced in 0.3.9
 	static migrateToCoreSort(entity) {
 		let order = entity.getFlag("entityorder", "order")
 		if (order !== undefined && order !== null) {
-			if (EntityOrder.migratingWarning !== true) {
-				// Show warning if we find that we need to migrate stuff.
-				// This can take quite a while since most probably every entity needs a database upgrade.
-				// Show the warning every 5 seconds until we stop migrating.
-				EntityOrder.migratingWarning = true;
-				console.warn("Migrating sort order from module to core sort key")
-				ui.notifications.warn("EntityOrder: Migrating sort order from module to core sort key.<p>Please be patient as this may take a while... This message will disappear when the migration is done.</p>")
-				setTimeout(() => EntityOrder.migratingWarning = false, 5000)
-			}
-			return entity.update({ sort: Math.floor(order), "flags.entityorder.-=order": null })
+			return {_id: entity.id, sort: Math.floor(order), "flags.entityorder.-=order": null }
 		}
-		return Promise.resolve(entity);
+		return null
+	}
+	static async migrateCollection(collection) {
+		let updateData = collection.entities.map(this.migrateToCoreSort).filter(u => u !== null);
+		if (updateData.length > 0)
+			return CONFIG[collection.entity].entityClass.updateMany(updateData);
+		return false;
 	}
 	static async migrate() {
-		let all_entities = game.journal.entities.concat(game.scenes.entities).concat(game.actors.entities).concat(game.items.entities)
-		for (let entity of all_entities)
-			await EntityOrder.migrateToCoreSort(entity).catch(() => { });
+		await EntityOrder.migrateCollection(game.journal);
+		await EntityOrder.migrateCollection(game.scenes);
+		await EntityOrder.migrateCollection(game.actors);
+		await EntityOrder.migrateCollection(game.items);
+		// Disabling self
+		let modules = game.settings.get('core', 'moduleConfiguration');
+		modules.entityorder = false;
+		game.settings.set('core', 'moduleConfiguration', modules);
 	}
 }
 
-Hooks.on('ready', EntityOrder.migrate)
-/* Add sort alphabetically option */
-Hooks.on('getJournalDirectoryFolderContext', EntityOrder.getEntityFolderContext);
-Hooks.on('getSceneDirectoryFolderContext', EntityOrder.getEntityFolderContext);
-Hooks.on('getActorDirectoryFolderContext', EntityOrder.getEntityFolderContext);
-Hooks.on('getItemDirectoryFolderContext', EntityOrder.getEntityFolderContext);
+Hooks.on('ready', EntityOrder.migrate);
